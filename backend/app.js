fae1a96069c93acbe0307c156913adef2382f862
Dare Goodness <daregoodness@gmail.com>
2023-04-10 18:41:29 +0100

    update openai

--- backend/app.js
+++ backend/app.js
@@ -18,20 +18,25 @@ app.get('/', (req, res) => {
 });
 app.post('/', async (req, res) => {
   const { prompt } = req.body;
+  let result;
 
   try {
     // const response = await openai.createCompletion({ prompt });
-    response = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 50,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
-    });
-
-    res.status(200).json({ status: 'success', data: response });
+    response = await openai
+      .createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${prompt}`,
+        temperature: 0,
+        max_tokens: 50,
+        top_p: 1,
+        frequency_penalty: 0.5,
+        presence_penalty: 0,
+      })
+      .then(res => {
+        console.log(res.data);
+        result = res.data;
+      });
+    res.status(200).json({ status: 'success', data: result });
   } catch (err) {
     console.log(err);
     res.status(500).json({ status: 'error', data: err });
